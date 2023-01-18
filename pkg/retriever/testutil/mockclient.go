5fadfc8de8ab66a939de672a3679ff3eaa72e057
Rod Vagg <rod@vagg.org>
2023-01-18 20:45:29 +1100

    chore: simulate and test Accepted and FirstByteReceived events

--- pkg/retriever/testutil/mockclient.go
+++ pkg/retriever/testutil/mockclient.go
@@ -12,8 +12,15 @@ import (
 	"github.com/filecoin-project/lassie/pkg/eventpublisher"
 	"github.com/filecoin-project/lassie/pkg/types"
 	"github.com/ipfs/go-cid"
+	"github.com/ipld/go-ipld-prime/datamodel"
 	"github.com/libp2p/go-libp2p/core/peer"
 )
+
+var ch1 = datatransfer.ChannelID{
+	Initiator: "initiator",
+	Responder: "responder",
+	ID:        1,
+}
 
 type DelayedQueryReturn struct {
 	QueryResponse *retrievalmarket.QueryResponse
@@ -75,9 +82,114 @@ func (dfc *MockClient) RetrieveFromPeer(
 		case <-time.After(drr.Delay):
 		}
 		eventsCallback(datatransfer.Event{Code: datatransfer.Open}, nil)
+		if drr.ResultStats != nil {
+			acceptedResponse := &retrievalmarket.DealResponse{
+				Status: retrievalmarket.DealStatusAccepted,
+			}
+			acceptedResponseNode := retrievalmarket.BindnodeRegistry.TypeToNode(acceptedResponse)
+			channelState := &mockChannelState{acceptedResponseNode, 100}
+			eventsCallback(datatransfer.Event{Code: datatransfer.NewVoucherResult}, channelState)
+			eventsCallback(datatransfer.Event{Code: datatransfer.DataReceivedProgress}, channelState)
+		}
 		return drr.ResultStats, drr.ResultErr
 	}
 	return nil, errors.New("nope")
 }
 
 func (*MockClient) SubscribeToRetrievalEvents(subscriber eventpublisher.RetrievalSubscriber) {}
+
+type mockChannelState struct {
+	lastVoucherResult datamodel.Node
+	received          uint64
+}
+
+func (m *mockChannelState) TransferID() datatransfer.TransferID {
+	panic("not implemented")
+}
+func (m *mockChannelState) BaseCID() cid.Cid {
+	panic("not implemented")
+}
+func (m *mockChannelState) Selector() datamodel.Node {
+	panic("not implemented")
+}
+func (m *mockChannelState) Voucher() datatransfer.TypedVoucher {
+	panic("not implemented")
+}
+func (m *mockChannelState) Sender() peer.ID {
+	panic("not implemented")
+}
+func (m *mockChannelState) Recipient() peer.ID {
+	panic("not implemented")
+}
+func (m *mockChannelState) TotalSize() uint64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) IsPull() bool {
+	panic("not implemented")
+}
+func (m *mockChannelState) ChannelID() datatransfer.ChannelID {
+	panic("not implemented")
+}
+func (m *mockChannelState) OtherPeer() peer.ID {
+	panic("not implemented")
+}
+func (m *mockChannelState) SelfPeer() peer.ID {
+	panic("not implemented")
+}
+func (m *mockChannelState) Status() datatransfer.Status {
+	panic("not implemented")
+}
+func (m *mockChannelState) Sent() uint64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) Received() uint64 {
+	return m.received
+}
+func (m *mockChannelState) Message() string {
+	panic("not implemented")
+}
+func (m *mockChannelState) Vouchers() []datatransfer.TypedVoucher {
+	panic("not implemented")
+}
+func (m *mockChannelState) VoucherResults() []datatransfer.TypedVoucher {
+	panic("not implemented")
+}
+func (m *mockChannelState) LastVoucher() datatransfer.TypedVoucher {
+	panic("not implemented")
+}
+func (m *mockChannelState) LastVoucherResult() datatransfer.TypedVoucher {
+	return datatransfer.TypedVoucher{Voucher: m.lastVoucherResult, Type: retrievalmarket.DealResponseType}
+}
+func (m *mockChannelState) ReceivedCidsTotal() int64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) QueuedCidsTotal() int64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) SentCidsTotal() int64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) Queued() uint64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) DataLimit() uint64 {
+	panic("not implemented")
+}
+func (m *mockChannelState) RequiresFinalization() bool {
+	panic("not implemented")
+}
+func (m *mockChannelState) InitiatorPaused() bool {
+	panic("not implemented")
+}
+func (m *mockChannelState) ResponderPaused() bool {
+	panic("not implemented")
+}
+func (m *mockChannelState) BothPaused() bool {
+	panic("not implemented")
+}
+func (m *mockChannelState) SelfPaused() bool {
+	panic("not implemented")
+}
+func (m *mockChannelState) Stages() *datatransfer.ChannelStages {
+	panic("not implemented")
+}
