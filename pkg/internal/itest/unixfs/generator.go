417f411806af59ac2d46b7f69d48743053db111a
Rod Vagg <rod@vagg.org>
2023-02-20 20:23:19 +1100

    feat: add unixfs sharded directory test case

--- pkg/internal/itest/unixfs/generator.go
+++ pkg/internal/itest/unixfs/generator.go
@@ -12,8 +12,10 @@ import (
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-unixfsnode/data/builder"
 	dagpb "github.com/ipld/go-codec-dagpb"
+	"github.com/ipld/go-ipld-prime"
 	"github.com/ipld/go-ipld-prime/linking"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/require"
 )
 
@@ -58,8 +60,8 @@ func rndInt(randReader io.Reader, max int) int {
 	return int(coin.Int64())
 }
 
-func GenerateDirectory(t *testing.T, linkSys *linking.LinkSystem, randReader io.Reader, targetSize int) (cid.Cid, []DirEntry) {
-	root, _, entries := generateDirectoryRecursive(t, linkSys, randReader, targetSize, "")
+func GenerateDirectory(t *testing.T, linkSys *linking.LinkSystem, randReader io.Reader, targetSize int, rootSharded bool) (cid.Cid, []DirEntry) {
+	root, _, entries := generateDirectoryRecursive(t, linkSys, randReader, targetSize, "", rootSharded)
 	return root, entries
 }
 
@@ -68,6 +70,7 @@ func generateDirectoryRecursive(t *testing.T,
 	randReader io.Reader,
 	targetSize int,
 	dir string,
+	sharded bool,
 ) (cid.Cid, int, []DirEntry) {
 
 	var curSize int
@@ -84,8 +87,7 @@ func generateDirectoryRecursive(t *testing.T,
 			// make a new directory
 			newDir, err := fileName(randReader)
 			require.NoError(t, err)
-			//fmt.Println("Dir:", newDir)
-			childRoot, childSize, childEntries := generateDirectoryRecursive(t, linkSys, randReader, targetSize, dir+"/"+newDir)
+			childRoot, childSize, childEntries := generateDirectoryRecursive(t, linkSys, randReader, targetSize, dir+"/"+newDir, false)
 			entries = append(entries, childEntries...)
 			lnk, err := builder.BuildUnixFSDirectoryEntry(newDir, int64(childSize), cidlink.Link{Cid: childRoot})
 			require.NoError(t, err)
@@ -108,14 +110,24 @@ func generateDirectoryRecursive(t *testing.T,
 				Cid:     root,
 			}
 			curSize += size
-			//fmt.Println("File:", entry.Path, len(entry.Content), targetSize-curSize)
 			entries = append(entries, entry)
 			lnk, err := builder.BuildUnixFSDirectoryEntry(name, int64(size), cidlink.Link{Cid: root})
 			require.NoError(t, err)
 			dirLinks = append(dirLinks, lnk)
 		}
 	}
-	root, size, err := builder.BuildUnixFSDirectory(dirLinks, linkSys)
-	require.NoError(t, err)
+	var root ipld.Link
+	var size uint64
+	var err error
+	if sharded {
+		// node arity of 16, quite small to increase collision probability so we actually get sharding
+		const width = 16
+		const hasher = multihash.MURMUR3X64_64
+		root, size, err = builder.BuildUnixFSShardedDirectory(width, hasher, dirLinks, linkSys)
+		require.NoError(t, err)
+	} else {
+		root, size, err = builder.BuildUnixFSDirectory(dirLinks, linkSys)
+		require.NoError(t, err)
+	}
 	return root.(cidlink.Link).Cid, int(size), entries
 }
