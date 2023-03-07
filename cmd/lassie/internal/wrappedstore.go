d47883ab4711ecc890cb13f18ffeb9dd1db1c383
Rod Vagg <rod@vagg.org>
2023-03-07 11:50:17 +1100

    feat: option to write the CAR to stdout
    also switches all existing fetch cmd output to stderr

--- cmd/lassie/internal/wrappedstore.go
+++ cmd/lassie/internal/wrappedstore.go
@@ -4,27 +4,30 @@ import (
 	"context"
 	"io"
 
-	carstore "github.com/ipld/go-car/v2/storage"
 	"github.com/ipld/go-ipld-prime/storage"
 )
 
+type ReadableWritableStorage interface {
+	storage.StreamingReadableStorage
+	storage.ReadableStorage
+	storage.WritableStorage
+}
+
 // putCbStore simply calls a callback on each put(), with the number of blocks put
-var _ storage.StreamingReadableStorage = (*putCbStore)(nil)
-var _ storage.ReadableStorage = (*putCbStore)(nil)
-var _ storage.WritableStorage = (*putCbStore)(nil)
+var _ ReadableWritableStorage = (*putCbStore)(nil)
 
 type putCbStore struct {
 	// parentOpener lazily opens the parent Store upon first call to this Store.
 	// This avoids Store instantiation until there is some interaction from the retriever.
 	// In the case of CARv2 Stores, this will avoid creation of empty .car files should
 	// the retriever fail to find any candidates.
-	parentOpener func() (*carstore.StorageCar, error)
+	parentOpener func() (ReadableWritableStorage, error)
 	// parent is lazily instantiated and should not be directly used; use parentStore instead.
-	parent *carstore.StorageCar
+	parent ReadableWritableStorage
 	cb     func(putCount int, putBytes int)
 }
 
-func NewPutCbStore(parentOpener func() (*carstore.StorageCar, error), cb func(putCount int, putBytes int)) *putCbStore {
+func NewPutCbStore(parentOpener func() (ReadableWritableStorage, error), cb func(putCount int, putBytes int)) *putCbStore {
 	return &putCbStore{parentOpener: parentOpener, cb: cb}
 }
 
@@ -35,6 +38,7 @@ func (pcb *putCbStore) Has(ctx context.Context, key string) (bool, error) {
 	}
 	return pbs.Has(ctx, key)
 }
+
 func (pcb *putCbStore) Get(ctx context.Context, key string) ([]byte, error) {
 	pbs, err := pcb.parentStore()
 	if err != nil {
@@ -42,6 +46,7 @@ func (pcb *putCbStore) Get(ctx context.Context, key string) ([]byte, error) {
 	}
 	return pbs.Get(ctx, key)
 }
+
 func (pcb *putCbStore) GetStream(ctx context.Context, key string) (io.ReadCloser, error) {
 	pbs, err := pcb.parentStore()
 	if err != nil {
@@ -49,6 +54,7 @@ func (pcb *putCbStore) GetStream(ctx context.Context, key string) (io.ReadCloser
 	}
 	return pbs.GetStream(ctx, key)
 }
+
 func (pcb *putCbStore) Put(ctx context.Context, key string, data []byte) error {
 	pbs, err := pcb.parentStore()
 	if err != nil {
@@ -57,14 +63,8 @@ func (pcb *putCbStore) Put(ctx context.Context, key string, data []byte) error {
 	pcb.cb(1, len(data))
 	return pbs.Put(ctx, key, data)
 }
-func (pcb *putCbStore) Finalize() error {
-	if pbs, err := pcb.parentStore(); err != nil {
-		return err
-	} else {
-		return pbs.Finalize()
-	}
-}
-func (pcb *putCbStore) parentStore() (*carstore.StorageCar, error) {
+
+func (pcb *putCbStore) parentStore() (ReadableWritableStorage, error) {
 	if pcb.parent == nil {
 		var err error
 		if pcb.parent, err = pcb.parentOpener(); err != nil {
