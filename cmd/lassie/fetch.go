ab674a181b668b2d686668c29c782fbbf08214a9
Rod Vagg <rod@vagg.org>
2023-05-09 20:42:23 +1000

    Revert "fix: ignore empty string env vars when we parse CLI flags (#208)"
    This reverts commit 7979b599044a78cb1b6247acd002309c947e2f40.

--- cmd/lassie/fetch.go
+++ cmd/lassie/fetch.go
@@ -75,11 +75,6 @@ var fetchCmd = &cli.Command{
 			DefaultText: "Providers will be discovered automatically",
 			Usage:       "Addresses of providers, including peer IDs, to use instead of automatic discovery, seperated by a comma. All protocols will be attempted when connecting to these providers. Example: /ip4/1.2.3.4/tcp/1234/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
 			Action: func(cctx *cli.Context, v string) error {
-				// Do nothing if given an empty string
-				if v == "" {
-					return nil
-				}
-
 				var err error
 				fetchProviderAddrInfos, err = types.ParseProviderStrings(v)
 				return err
