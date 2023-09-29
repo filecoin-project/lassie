b33092c915605bc9f87cf99f413e5a0630abfe4b
Rod Vagg <rod@vagg.org>
2023-09-29 17:40:29 +1000

    feat: allow missing peer ID & http:// url style on HTTP --providers
    Where peer ID is not provided, we generate a random one for internal use.
    
    Ref: https://github.com/filecoin-project/lassie/issues/418

--- pkg/types/request_test.go
+++ pkg/types/request_test.go
@@ -1,9 +1,8 @@
-package types_test
+package types
 
 import (
 	"testing"
 
-	"github.com/filecoin-project/lassie/pkg/types"
 	"github.com/ipfs/go-cid"
 	trustlessutils "github.com/ipld/go-trustless-utils"
 	"github.com/multiformats/go-multicodec"
@@ -18,13 +17,13 @@ func TestRequestStringRepresentations(t *testing.T) {
 
 	testCases := []struct {
 		name               string
-		request            types.RetrievalRequest
+		request            RetrievalRequest
 		expectedUrlPath    string
 		expectedDescriptor string
 	}{
 		{
 			name: "plain",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{Root: testCidV1},
 			},
 			expectedUrlPath:    "?dag-scope=all",
@@ -32,7 +31,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "path",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{Root: testCidV1, Path: "/some/path/to/thing"},
 			},
 			expectedUrlPath:    "/some/path/to/thing?dag-scope=all",
@@ -40,7 +39,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "escaped path",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{Root: testCidV1, Path: "/?/#/;/&/ /!"},
 			},
 			expectedUrlPath:    "/%3F/%23/%3B/&/%20/%21?dag-scope=all",
@@ -48,7 +47,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "entity",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{Root: testCidV1, Scope: trustlessutils.DagScopeEntity},
 			},
 			expectedUrlPath:    "?dag-scope=entity",
@@ -56,7 +55,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "block",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{Root: testCidV1, Scope: trustlessutils.DagScopeBlock},
 			},
 			expectedUrlPath:    "?dag-scope=block",
@@ -64,7 +63,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "protocol",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request:   trustlessutils.Request{Root: testCidV0},
 				Protocols: []multicodec.Code{multicodec.TransportGraphsyncFilecoinv1},
 			},
@@ -73,7 +72,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "protocols",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request:   trustlessutils.Request{Root: testCidV1},
 				Protocols: []multicodec.Code{multicodec.TransportBitswap, multicodec.TransportIpfsGatewayHttp},
 			},
@@ -82,7 +81,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "duplicates",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{Root: testCidV0, Duplicates: true},
 			},
 			expectedUrlPath:    "?dag-scope=all",
@@ -90,7 +89,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "block limit",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request:   trustlessutils.Request{Root: testCidV1},
 				MaxBlocks: 100,
 			},
@@ -99,25 +98,25 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "fixed peer",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request:    trustlessutils.Request{Root: testCidV1},
-				FixedPeers: must(types.ParseProviderStrings("/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4")),
+				FixedPeers: must(ParseProviderStrings("/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4")),
 			},
 			expectedUrlPath:    "?dag-scope=all",
 			expectedDescriptor: "/ipfs/bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi?dag-scope=all&dups=n&providers=/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
 		},
 		{
 			name: "fixed peers",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request:    trustlessutils.Request{Root: testCidV1},
-				FixedPeers: must(types.ParseProviderStrings("/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4")),
+				FixedPeers: must(ParseProviderStrings("/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4")),
 			},
 			expectedUrlPath:    "?dag-scope=all",
 			expectedDescriptor: "/ipfs/bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi?dag-scope=all&dups=n&providers=/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
 		},
 		{
 			name: "byte range",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{
 					Root:  testCidV1,
 					Bytes: &trustlessutils.ByteRange{From: 100, To: ptr(200)},
@@ -128,7 +127,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "byte range -ve",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{
 					Root:  testCidV1,
 					Bytes: &trustlessutils.ByteRange{From: -100},
@@ -139,7 +138,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 		},
 		{
 			name: "all the things",
-			request: types.RetrievalRequest{
+			request: RetrievalRequest{
 				Request: trustlessutils.Request{
 					Root:       testCidV0,
 					Path:       "/some/path/to/thing",
@@ -149,7 +148,7 @@ func TestRequestStringRepresentations(t *testing.T) {
 				},
 				MaxBlocks:  222,
 				Protocols:  []multicodec.Code{multicodec.TransportBitswap, multicodec.TransportIpfsGatewayHttp},
-				FixedPeers: must(types.ParseProviderStrings("/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4")),
+				FixedPeers: must(ParseProviderStrings("/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4")),
 			},
 			expectedUrlPath:    "/some/path/to/thing?dag-scope=entity&entity-bytes=100:-200",
 			expectedDescriptor: "/ipfs/QmVXsSVjwxMsCwKRCUxEkGb4f4B98gXVy3ih3v4otvcURK/some/path/to/thing?dag-scope=entity&entity-bytes=100:-200&dups=y&blockLimit=222&protocols=transport-bitswap,transport-ipfs-gateway-http&providers=/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
@@ -165,6 +164,97 @@ func TestRequestStringRepresentations(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, tc.expectedDescriptor, actual)
 		})
+	}
+
+	t.Run("fixed peer, no peer ID", func(t *testing.T) {
+		pps, err := ParseProviderStrings("/ip4/127.0.0.1/tcp/5000/http")
+		require.NoError(t, err)
+		request := RetrievalRequest{
+			Request:    trustlessutils.Request{Root: testCidV1},
+			FixedPeers: pps,
+		}
+		ds, err := request.GetDescriptorString()
+		require.NoError(t, err)
+		expectedStart := "/ipfs/bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi?dag-scope=all&dups=n&providers=/ip4/127.0.0.1/tcp/5000/http/p2p/1TunknownX"
+		require.Equal(t, expectedStart, ds[0:len(expectedStart)])
+	})
+
+	t.Run("fixed peer, http:// URL", func(t *testing.T) {
+		pps, err := ParseProviderStrings("http://127.0.0.1:5000")
+		require.NoError(t, err)
+		request := RetrievalRequest{
+			Request:    trustlessutils.Request{Root: testCidV1},
+			FixedPeers: pps,
+		}
+		ds, err := request.GetDescriptorString()
+		require.NoError(t, err)
+		expectedStart := "/ipfs/bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi?dag-scope=all&dups=n&providers=/ip4/127.0.0.1/tcp/5000/http/p2p/1TunknownX"
+		require.Equal(t, expectedStart, ds[0:len(expectedStart)])
+	})
+}
+
+func TestProviderStrings(t *testing.T) {
+	testCases := []struct {
+		name        string
+		input       string
+		expectMatch string // regex
+		expectErr   string
+	}{
+		{
+			name:      "empty",
+			input:     "",
+			expectErr: "failed to parse multiaddr",
+		},
+		{
+			name:        "single",
+			input:       "/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg",
+			expectMatch: "^/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg$",
+		},
+		{
+			name:        "multi",
+			input:       "/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/dns4/dag.w3s.link/tcp/443/https/p2p/QmUA9D3H7HeCYsirB3KmPSvZh3dNXMZas6Lwgr4fv1HTTp",
+			expectMatch: "^/dns/beep.boop.com/tcp/3747/p2p/12D3KooWDXAVxjSTKbHKpNk8mFVQzHdBDvR4kybu582Xd4Zrvagg,/dns4/dag.w3s.link/tcp/443/https/p2p/QmUA9D3H7HeCYsirB3KmPSvZh3dNXMZas6Lwgr4fv1HTTp$",
+		},
+		{
+			name:        "no id",
+			input:       "/ip4/127.0.0.1/tcp/5000/http",
+			expectMatch: "^/ip4/127.0.0.1/tcp/5000/http/p2p/1TunknownX",
+		},
+		{
+			name:        "http:// form",
+			input:       "http://127.0.0.1:5000",
+			expectMatch: "^/ip4/127.0.0.1/tcp/5000/http/p2p/1TunknownX",
+		},
+		{
+			name:      "http:// form with path err",
+			input:     "http://127.0.0.1:5000/boop",
+			expectErr: "invalid provider URL, paths not supported:",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			parsed, err := ParseProviderStrings(tc.input)
+			if tc.expectErr != "" {
+				require.Contains(t, err.Error(), tc.expectErr)
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			actual, err := ToProviderString(parsed)
+			require.NoError(t, err)
+
+			require.Regexp(t, tc.expectMatch, actual)
+		})
+	}
+}
+
+func TestUnknownPeerID(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		p := nextUnknownPeerID()
+		require.Equal(t, "1TunknownX", p.String()[0:10])
+		require.True(t, IsUnknownPeerID(p))
 	}
 }
 
