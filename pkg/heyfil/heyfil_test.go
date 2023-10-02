bc241ceb44b2c5893e56350794a1bc39c4bfb1ae
Rod Vagg <rod@vagg.org>
2023-10-02 16:33:58 +1100

    fix: don't use heyfil for peer IDs
    disabled in code, but can still be done in the heyfil package

--- pkg/heyfil/heyfil_test.go
+++ pkg/heyfil/heyfil_test.go
@@ -11,27 +11,30 @@ import (
 
 func TestCanHeyfil(t *testing.T) {
 	testCase := []struct {
-		name  string
-		input string
-		can   bool
+		name          string
+		input         string
+		canWithPeerID bool
+		canWithFaddr  bool
 	}{
-		{"empty", "", false},
-		{"faddr", "f01234", true},
-		{"faddr non miner", "f1234", false},
-		{"bad faddr", "f00cafebeef", false},
-		{"p2p", "12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4", true},
-		{"cid p2p", "QmUA9D3H7HeCYsirB3KmPSvZh3dNXMZas6Lwgr4fv1HTTp", true},
-		{"cidv1 p2p", "bafzbeicwot2npbkuyjppqaoibbohqemn5dnbidt66mjdfso725vm4lmmlm", true},
-		{"cidv1 not p2p", "bafybeicwot2npbkuyjppqaoibbohqemn5dnbidt66mjdfso725vm4lmmlm", false},
-		{"multiaddr", "/dns4/dag.w3s.link/tcp/443/https", false},
-		{"http addr", "http://dag.w3s.link:443", false},
-		{"multiaddr long", "/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4", false},
-		{"multiaddr ip4", "/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA", false},
+		{"empty", "", false, false},
+		{"faddr", "f01234", false, true},
+		{"faddr non miner", "f1234", false, false},
+		{"bad faddr", "f00cafebeef", false, false},
+		{"p2p", "12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4", true, false},
+		{"cid p2p", "QmUA9D3H7HeCYsirB3KmPSvZh3dNXMZas6Lwgr4fv1HTTp", true, false},
+		{"cidv1 p2p", "bafzbeicwot2npbkuyjppqaoibbohqemn5dnbidt66mjdfso725vm4lmmlm", true, false},
+		{"cidv1 not p2p", "bafybeicwot2npbkuyjppqaoibbohqemn5dnbidt66mjdfso725vm4lmmlm", false, false},
+		{"multiaddr", "/dns4/dag.w3s.link/tcp/443/https", false, false},
+		{"http addr", "http://dag.w3s.link:443", false, false},
+		{"multiaddr long", "/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4", false, false},
+		{"multiaddr ip4", "/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA", false, false},
 	}
 
 	for _, tc := range testCase {
 		t.Run(tc.name, func(t *testing.T) {
-			require.Equal(t, heyfil.CanHeyfil(tc.input), tc.can)
+			require.False(t, heyfil.Heyfil{}.CanTranslate(tc.input))
+			require.Equal(t, heyfil.Heyfil{TranslatePeerId: true}.CanTranslate(tc.input), tc.canWithPeerID)
+			require.Equal(t, heyfil.Heyfil{TranslateFaddr: true}.CanTranslate(tc.input), tc.canWithFaddr)
 		})
 	}
 }
@@ -42,9 +45,15 @@ func TestHeyfil(t *testing.T) {
 
 	trans, err := heyfil.Heyfil{Endpoint: ts.URL}.Translate("12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ")
 	require.NoError(t, err)
+	require.Equal(t, "12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ", trans)
+	trans, err = heyfil.Heyfil{Endpoint: ts.URL, TranslatePeerId: true}.Translate("12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ")
+	require.NoError(t, err)
 	require.Equal(t, "/ip4/85.11.148.122/tcp/24001/p2p/12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ", trans)
 
 	trans, err = heyfil.Heyfil{Endpoint: ts.URL}.Translate("f0127896")
+	require.NoError(t, err)
+	require.Equal(t, "f0127896", trans)
+	trans, err = heyfil.Heyfil{Endpoint: ts.URL, TranslateFaddr: true}.Translate("f0127896")
 	require.NoError(t, err)
 	require.Equal(t, "/ip4/85.11.148.122/tcp/24001/p2p/12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ", trans)
 
@@ -56,7 +65,7 @@ func TestHeyfil(t *testing.T) {
 		"/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA",
 		"WOT?", // this is an error for another layer ...
 	} {
-		trans, err := heyfil.Heyfil{Endpoint: ts.URL}.Translate(inp)
+		trans, err := heyfil.Heyfil{Endpoint: ts.URL, TranslatePeerId: true, TranslateFaddr: true}.Translate(inp)
 		require.NoError(t, err)
 		require.Equal(t, trans, inp)
 	}
@@ -66,48 +75,60 @@ func TestHeyfilTranslateAll(t *testing.T) {
 	ts := newHeyfilServer()
 	defer ts.Close()
 
-	input := []string{
-		"/dns4/dag.w3s.link/tcp/443/https",
-		"http://dag.w3s.link:443",
-		"12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ",
-		"/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
-		"/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA",
-		"f0127896",
-		"WOT?", // this is an error for another layer ...
+	testData := []struct {
+		addr     string
+		want     string
+		ispeerid bool
+		isfaddr  bool
+	}{
+		{"/dns4/dag.w3s.link/tcp/443/https", "/dns4/dag.w3s.link/tcp/443/https", false, false},
+		{"http://dag.w3s.link:443", "http://dag.w3s.link:443", false, false},
+		{"12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ", "/ip4/85.11.148.122/tcp/24001/p2p/12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ", true, false},
+		{"/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4", "/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4", false, false},
+		{"/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA", "/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA", false, false},
+		{"f0127896", "/ip4/85.11.148.122/tcp/24001/p2p/12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ", false, true},
+		{"WOT?", "WOT?", false, false}, // this is an error for another layer ..."WOT?"
 	}
-	expected := []string{
-		"/dns4/dag.w3s.link/tcp/443/https",
-		"http://dag.w3s.link:443",
-		"/ip4/85.11.148.122/tcp/24001/p2p/12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ",
-		"/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
-		"/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA",
-		"/ip4/85.11.148.122/tcp/24001/p2p/12D3KooWE8yt84RVwW3sFcd6WMjbUdWrZer2YtT4dmtj3dHdahSZ",
-		"WOT?",
+	input := make([]string, len(testData))
+	for ii, td := range testData {
+		input[ii] = td.addr
 	}
 
 	trans, err := heyfil.Heyfil{Endpoint: ts.URL}.TranslateAll(input)
 	require.NoError(t, err)
-	require.Equal(t, trans, expected)
-
-	// same but nothing to translate, make sure we can pass through without even trying
-	input = []string{
-		"/dns4/dag.w3s.link/tcp/443/https",
-		"http://dag.w3s.link:443",
-		"/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
-		"/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA",
-		"WOT?", // this is an error for another layer ...
-	}
-	expected = []string{
-		"/dns4/dag.w3s.link/tcp/443/https",
-		"http://dag.w3s.link:443",
-		"/dns4/dag.w3s.link/tcp/443/https/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA4",
-		"/ip4/127.0.0.1/tcp/5000/p2p/12D3KooWBSTEYMLSu5FnQjshEVah9LFGEZoQt26eacCEVYfedWA",
-		"WOT?",
+	for ii, td := range testData {
+		want := td.want
+		if td.isfaddr || td.ispeerid {
+			want = td.addr
+		}
+		require.Equal(t, trans[ii], want)
 	}
 
-	trans, err = heyfil.Heyfil{Endpoint: ts.URL}.TranslateAll(input)
+	trans, err = heyfil.Heyfil{Endpoint: ts.URL, TranslatePeerId: true}.TranslateAll(input)
 	require.NoError(t, err)
-	require.Equal(t, trans, expected)
+	for ii, td := range testData {
+		want := td.want
+		if td.isfaddr {
+			want = td.addr
+		}
+		require.Equal(t, trans[ii], want)
+	}
+
+	trans, err = heyfil.Heyfil{Endpoint: ts.URL, TranslateFaddr: true}.TranslateAll(input)
+	require.NoError(t, err)
+	for ii, td := range testData {
+		want := td.want
+		if td.ispeerid {
+			want = td.addr
+		}
+		require.Equal(t, trans[ii], want)
+	}
+
+	trans, err = heyfil.Heyfil{Endpoint: ts.URL, TranslateFaddr: true, TranslatePeerId: true}.TranslateAll(input)
+	require.NoError(t, err)
+	for ii, td := range testData {
+		require.Equal(t, trans[ii], td.want)
+	}
 }
 
 func newHeyfilServer() *httptest.Server {
